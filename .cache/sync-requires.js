f35c3159fc4398d876bc8c0a6d85e35056d153f5
Mateusz Myśliwiec <mateuszmysliwiec03@gmail.com>
2022-11-24 14:05:38 +0100

    gatsby-config

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\mateu\\OneDrive\\Pulpit\\pixel-perfect-project\\.cache\\dev-404-page.js")),
   "component---src-pages-index-js": preferDefault(require("C:\\Users\\mateu\\OneDrive\\Pulpit\\pixel-perfect-project\\src\\pages\\index.js"))
 }
 
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  siteMetadata: {
+    title: `Gatsby`,
+  },
   plugins: [
     `gatsby-plugin-sass`,
 
